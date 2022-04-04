1d02f1fca1035839b835322ea1058f6d7ef36108
Prathmesh Chhabra <p.chhabra2002@gmail.com>
2022-04-05 01:41:43 +0530

    Project Finished

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const fs = require("fs");
 const http = require("http");
 const url = require("url");
+const slugify = require("slugify");
+const replaceTemplate = require("./modules/replaceTemplate");
 
 /*
 const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
@@ -33,19 +35,6 @@ console.log("reading file");
 ///Prefer asynchronus code over synchronus to avoid blocking
 
 ///SERVER
-const replaceTemplate = (temp, product) => {
-  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%ID%}/g, product.id);
-
-  if (!product.organic) output.replace(/{%NOT_ORGANIC%}/g, `not-organic`);
-  return output;
-};
 
 const tempOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
@@ -62,6 +51,10 @@ const tempProduct = fs.readFileSync(
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
+
+const slugs = dataObj.map((el) => slugify(el.productName, { lower: true }));
+console.log(slugs);
+console.log(slugify("fresh avocados", { lower: true }));
 
 const server = http.createServer((req, res) => {
   console.log(req.url);
